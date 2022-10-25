b5a2f8d6108b03f2b2031521066be48078573ccb
Smit <susmit025@gmail.com>
2022-10-25 17:31:38 +0530

    entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "${env.SONAR_TOKEN}" ]]; then
+  echo "Set the SONAR_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -f "pom.xml" ]]; then
+  echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
+  exit 1
+fi
+
+if [[ -f "build.gradle" ]]; then
+  echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
+  exit 1
+fi
+
+if [[ -z "${SONARCLOUD_URL}" ]]; then
+  SONARCLOUD_URL="https://sonarcloud.io"
+fi
+
+sonar-scanner -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} -Dsonar.host.url=${SONARCLOUD_URL} ${INPUT_ARGS}
